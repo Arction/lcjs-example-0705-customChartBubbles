c84ab70a9a9093ee20f7bab6f10ef1f704157bc3
Ilkka Kuosmanen <ilkka.kuosmanen@arction.com>
2019-08-05 13:42:34 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,74 @@
+# Customized chart - Bubbles
+
+This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
+
+LightningChart JS is entirely GPU accelerated and performance optimized charting library for presenting massive amounts of data. It offers an easy way of creating sophisticated and interactive charts and adding them to your website or web application.
+
+The demo can be used as an example or a seed project. Local execution requires the following steps:
+
+- Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+- Open the project folder in a terminal:
+
+        npm install              # fetches dependencies
+        npm start                # builds an application and starts the development server
+
+- The application is available at *http://localhost:8080* in your browser, webpack-dev-server provides hot reload functionality.
+
+### Description
+
+*Also known as a Bubble Series, Bubble Chart, and Bubble Graph*
+
+This example shows extensive customization of a Cartesian Chart to create a Bubble Chart using ***EllipseSeries*** as a tool.
+
+The ***Bubble chart*** is a variation of a Scatter series where the data points have additional third dimension represented in the size of the markers ( ***bubbles*** ).
+
+This type of series are often used to present financial or statistical data because they the easiest and well-understood visual comparisons of measures.
+
+The series typically accepts points in format `{ x: number, y: number, size: number }`.
+
+### API links
+
+* Cartesian chart: [ChartXY][]
+* Axis: [Axis][]
+* Ellipse series: [EllipseSeries][]
+* Font settings: [FontSettings][]
+* RGBA color factory: [ColorRGBA][]
+* Solid FillStyle: [SolidFill][]
+* Solid LineStyle: [SolidLine][]
+* Empty FillStyle: [emptyFill][]
+* UI position origins: [UIOrigins][]
+* UI Dragging modes: [UIDraggingModes][]
+* Animator: [Animator][]
+
+
+### Support
+
+If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
+
+Official [API documentation][1] can be found on [Arction][2] website.
+
+If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
+
+If you think you found a bug in the LightningChart JavaScript library, please contact support@arction.com.
+
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@arction.com.
+
+© Arction Ltd 2009-2019. All rights reserved.
+
+[0]: https://github.com/Arction/
+[1]: https://www.arction.com/lightningchart-js-api-documentation/
+[2]: https://www.arction.com
+[3]: https://stackoverflow.com/questions/tagged/lightningchart
+[4]: https://www.arction.com/support-services/
+
+[Animator]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/globals.html#animator
+[Axis]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/axis.html
+[ChartXY]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/chartxy.html
+[ColorRGBA]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/globals.html#colorrgba
+[EllipseSeries]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/ellipseseries.html
+[emptyFill]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/globals.html#emptyfill
+[FontSettings]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/fontsettings.html
+[SolidFill]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/solidfill.html
+[SolidLine]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/solidline.html
+[UIDraggingModes]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/enums/uidraggingmodes.html
+[UIOrigins]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/globals.html#uiorigins
---
+++ src/index.js
@@ -0,0 +1,171 @@
+/**
+ * LightningChartJS example that showcases a extensively customized chart with Bubble-chart.
+ */
+// Import LightningChartJS
+const lcjs = require('@arction/lcjs')
+
+// Extract required parts from LightningChartJS.
+const {
+    lightningChart,
+    SolidFill,
+    SolidLine,
+    ColorRGBA,
+    emptyFill,
+    emptyTick,
+    FontSettings,
+    AutoCursorModes,
+    Animator,
+    AnimationEasings,
+    UIDraggingModes,
+    UIOrigins,
+    ColorPalettes
+} = lcjs
+
+// Custom callback template.
+const forEachIn = (object, clbk) => { const obj = {}; for (const a in object) obj[a] = clbk(object[a]); return obj }
+
+// Define colors to configure chart and bubbles.
+const colors = {
+    background: ColorRGBA(255, 255, 255),
+    graphBackground: ColorRGBA(220, 255, 255),
+    title: ColorRGBA(0, 100, 0),
+    subTitle: ColorRGBA(0, 100, 0),
+    bubbleBorder: ColorRGBA(0, 0, 0),
+    bubbleFillPalette: ColorPalettes.fullSpectrum(100)
+}
+
+// Define font settings.
+const fonts = {
+    title: new FontSettings({
+        size: 40,
+        weight: 400
+    })
+}
+// Create and subtitle with the same font settings, except font-size.
+fonts.subTitle = fonts.title.setSize(20)
+
+// Create solid fill styles for defined colors.
+const solidFillStyles = forEachIn(colors, (color) => new SolidFill({ color }))
+
+// Create chart with customized settings
+const chart = lightningChart().ChartXY({})
+    .setBackgroundFillStyle(solidFillStyles.background)
+    .setChartBackgroundFillStyle(solidFillStyles.graphBackground)
+    .setTitle('Custom Styled Chart')
+    .setTitleFont(fonts.title)
+    .setTitleFillStyle(solidFillStyles.title)
+    .setTitleMarginTop(6)
+    .setTitleMarginBottom(0)
+    .setPadding({ left: 5, right: 5, top: 30, bottom: 30 })
+    .setAutoCursorMode(AutoCursorModes.disabled)
+    .setMouseInteractionRectangleZoom(undefined)
+    .setMouseInteractionRectangleFit(undefined)
+    .setMouseInteractions(false)
+
+// Get axes.
+const axes = {
+    bottom: chart.getDefaultAxisX(),
+    left: chart.getDefaultAxisY(),
+    top: chart.addAxisX(true),
+    right: chart.addAxisY(true).setChartInteractions(false)
+}
+
+chart.addUIElement(undefined, { x: chart.uiScale.x, y: axes.right.scale })
+    .setPosition({ x: 50, y: 10 })
+    .setOrigin(UIOrigins.CenterBottom)
+    .setMargin({ bottom: 10 })
+    .setText('- With Bubbles -')
+    .setFont(fonts.subTitle)
+    .setTextFillStyle(solidFillStyles.subTitle)
+    .setDraggingMode(UIDraggingModes.notDraggable)
+
+// Axis mutator.
+const overrideAxis = (axis) => axis
+    .setTickStyle(emptyTick)
+    .setTitleMargin(0)
+    .setNibStyle(line => line.setFillStyle(emptyFill))
+    .setMouseInteractions(undefined)
+
+// Override default configurations of axes.
+for (const axisPos in axes)
+    overrideAxis(axes[axisPos]);
+
+[axes.bottom, axes.left].forEach(axis => axis.setInterval(-100, 100).setScrollStrategy(undefined))
+const bubblePx = {
+    x: axes.bottom.scale.getPixelSize(),
+    y: axes.left.scale.getPixelSize()
+}
+
+// Create instance of ellipse series to draw bubbles.
+const ellipseSeries = chart.addEllipseSeries()
+let bubbleCount = 0
+
+// Handler of dragging bubbles.
+const bubbleDragHandler = (figure, event, button, startLocation, delta) => {
+    const prevDimensions = figure.getDimensions()
+    figure.setDimensions(Object.assign(prevDimensions, {
+        x: prevDimensions.x + delta.x * figure.scale.x.getPixelSize(),
+        y: prevDimensions.y + delta.y * figure.scale.y.getPixelSize()
+    }))
+}
+
+// Create resizeBubble array and sizeArray to store the values separately 
+const resizeBubble = []
+const sizeArray = []
+
+// Create a single bubble to visualize in specific coordinates and specified size.
+const addBubble = (pos, size) => {
+    const radius = size * 10
+    const borderThickness = 1 + size * 1.0
+
+    const color = colors.bubbleFillPalette(Math.round(Math.random() * 99))
+    const fillStyle = new SolidFill({ color })
+    const strokeStyle = new SolidLine({ fillStyle: solidFillStyles.bubbleBorder, thickness: borderThickness })
+
+    const figure = ellipseSeries.add({
+        x: pos.x,
+        y: pos.y,
+        radiusX: radius * bubblePx.x,
+        radiusY: radius * bubblePx.y
+    })
+        .setFillStyle(fillStyle)
+        .setStrokeStyle(strokeStyle)
+
+    // Make draggable by mouse.
+    figure.onMouseDrag(bubbleDragHandler)
+    bubbleCount++
+    return figure
+}
+
+// Create an event to handle the case when user resizes the widnow, the bubble will be automatically scaled 
+chart.onResize(() => {
+    for (let i = 0; i <= bubbleMaxCount - 1; i++) {
+        const newBubble = resizeBubble[i].getDimensions()
+        resizeBubble[i].setDimensions({
+            x: newBubble.x,
+            y: newBubble.y,
+            radiusX: axes.bottom.scale.getPixelSize() * sizeArray[i] * 10,
+            radiusY: axes.left.scale.getPixelSize() * sizeArray[i] * 10
+        })
+    }
+})
+
+// Create a single bubble to visualize in random coordinates and with random size.
+const addRandomBubble = () => {
+    const pos = {
+        x: Math.random() * 200 - 100,
+        y: Math.random() * 200 - 100
+    }
+    const size = 1 + Math.random() * 7.0
+    sizeArray.push(size)
+    resizeBubble.push(addBubble(pos, size))
+}
+
+// Amount of bubbles to render.
+const bubbleMaxCount = 100
+
+// Animate bubbles creation.
+Animator(() => undefined)(2.5 * 1000, AnimationEasings.ease)([[0, bubbleMaxCount]], ([nextBubbleCount]) => {
+    while (bubbleCount < nextBubbleCount)
+        addRandomBubble()
+})

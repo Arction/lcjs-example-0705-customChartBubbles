0959e408dd85c82e5ff2664d4a6bb34b6ba37862
snekw <dev.snekw@gmail.com>
2021-12-01 12:47:05 +0200

    Update LCJS version

--- src/index.js
+++ src/index.js
@@ -147,7 +147,7 @@ const addBubble = (pos, size) => {
 
 // Create an event to handle the case when user resizes the window, the bubble will be automatically scaled 
 chart.onResize(() => {
-    for (let i = 0; i <= bubbleMaxCount - 1; i++) {
+    for (let i = 0; i < resizeBubble.length; i++) {
         const newBubble = resizeBubble[i].getDimensions()
         resizeBubble[i].setDimensions({
             x: newBubble.x,
